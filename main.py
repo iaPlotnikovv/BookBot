8678d472abd60b8e19e8784b6f030b2cc18873b7
iaPlotnikovv <idoffix@gmail.com>
2024-09-14 21:56:53 +0300

    add report of counted words and chars

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 
 def main() :    
-    path = "./books/frankenstein.txt"
+    path = "books/frankenstein.txt"
     text =print_the_text(path)
-    counted_words = word_counter(text)
-    counted_char = character_counter(text)
-   # print(text)
-    print(f"in this text {counted_words} words!..\n..and i've counted every character for you! Why? Just for fun! U R WELCOME!!!\n{counted_char}")
+    words = word_counter(text)
+    chars = character_counter(text)
+    #print(text)
+    report(path,words,chars)
+   
     
+def report(p,w,c):
+    print(f"--- REPORT OF {p} ---\n {w} words found in this document\n")
+    for key,value in c:
+            print(f"The '{key}' character was found {value} times")
+    print("--- END REPORT ---")
+
 
 def print_the_text(path):
     with open(path) as f:       
@@ -22,9 +29,13 @@ def character_counter(text):
         char = char.lower()
         if char in counter:                
             counter[char]+=1
-        else:
+        elif char.isalpha()==True:
             counter[char]=1
-    return counter
+    
+        #sorting operation
+    sorted_counter= sorted(counter.items(),reverse=True, key=lambda pair:pair[1] )
+
+    return sorted_counter
 
 
 main()
